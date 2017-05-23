2948f97908422ae53bb96284057e78c8691031d6
Mark <mark@oben.com>
2017-05-23 15:26:01 -0700

    wrote simple method for grabbing ticker data and making tsv

---
+++ mjh/crypto/coin_market_cap_api.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def get_ticker():
+    r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
+    json_response = r.json()
+
+    n_currencies = len(json_response)
+
+    output_filename = '/Users/mharvill/Desktop/crypto_currency_data.tsv'
+
+    with open(output_filename, 'w') as f:
+        f.write('symbol\tname\tprice (USD)\t1-hour change\t24-hour change\t7-day change\t\n')
+    for currency_index in range(n_currencies):
+        current_json_response = json_response[currency_index]
+        symbol = current_json_response['symbol']
+        name = current_json_response['id']
+        price = current_json_response['price_usd']
+        change_1hr = current_json_response['percent_change_1h']
+        change_24hr = current_json_response['percent_change_24h']
+        change_7d = current_json_response['percent_change_7d']
+        with open(output_filename, 'a') as f:
+            f.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(symbol, name, price, change_1hr, change_24hr, change_7d))
+
+if __name__ == "__main__":
+    get_ticker()

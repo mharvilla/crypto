0159d23d4b7c45ca3f34d288dc276f88359dec4b
Mark <mark@oben.com>
2017-05-24 14:01:35 -0700

    make filename an input argument; implement very simple command line interface

--- mjh/crypto/coin_market_cap_api.py
+++ mjh/crypto/coin_market_cap_api.py
@@ -1,13 +1,17 @@
 import requests
+import sys
 
 
-def get_ticker():
+def get_ticker(output_filename):
+    if not isinstance(output_filename, str):
+        raise TypeError('output filename must be a text string')
+
     r = requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
     json_response = r.json()
 
     n_currencies = len(json_response)
 
-    output_filename = '/Users/mharvill/Desktop/crypto_currency_data.tsv'
+    # output_filename = '/Users/mharvill/Desktop/crypto_currency_data.tsv'
 
     with open(output_filename, 'w') as f:
         f.write('symbol\tname\tprice (USD)\t1-hour change (%)\t24-hour change (%)\t7-day change (%)\t\n')
@@ -24,4 +28,4 @@ def get_ticker():
             f.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(symbol, name, price, change_1hr, change_24hr, change_7d))
 
 if __name__ == "__main__":
-    get_ticker()
+    get_ticker(sys.argv[1])
